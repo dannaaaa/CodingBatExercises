27f0f9d66a44e5d4f3a2b5e8c00815faf90be4c7
Dana <danaawua@hotmail.co.uk>
2020-02-19 15:44:11 +0000

    added methods to main to run exercises

--- src/main/java/com/spartagloal/CodingBatExercises/App.java
+++ src/main/java/com/spartagloal/CodingBatExercises/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayCount9 arrayCount9 = new ArrayCount9();
+        System.out.println("There are " + arrayCount9.arrayCount9(new int[] {2,8,9,3,99,9}) + " nines in the array");
+
+        ArrayFront9 arrayFront9 = new ArrayFront9();
+        System.out.println(arrayFront9.arrayFront9(new int[] {1, 2, 9, 4}));
+
+        BigDiff bigDiff = new BigDiff();
+        System.out.println(bigDiff.bigDiff(new int [] {1, 3, 5,8}));
     }
+
+
+
 }
